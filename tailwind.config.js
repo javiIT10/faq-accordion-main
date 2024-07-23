0f778e4c63d4e17d840819a1cd2cdf491884d28e
Javier <javier.duarte.iti@gmail.com>
2024-07-23 11:06:54 -0500

    Añadiendo Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "work-sans": ["Work Sans", "sans-serif"],
+      },
+      colors: {
+        /* ## Colors */
+        white: "hsl(0, 0%, 100%)",
+        "light-pink": "hsl(275, 100%, 97%)",
+        "grayish-purple": "hsl(292, 16%, 49%)",
+        "dark-purple": "hsl(292, 42%, 14%)",
+      },
+    },
+  },
+  plugins: [],
+};
