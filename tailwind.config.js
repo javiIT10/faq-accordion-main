f2af877b68b29cd1d39d83390eaf1c1b6282bf08
Javier <javier.duarte.iti@gmail.com>
2024-07-23 12:08:59 -0500

    Generando HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       colors: {
         /* ## Colors */
         white: "hsl(0, 0%, 100%)",
+        pink: "#B12BEE",
         "light-pink": "hsl(275, 100%, 97%)",
         "grayish-purple": "hsl(292, 16%, 49%)",
         "dark-purple": "hsl(292, 42%, 14%)",
